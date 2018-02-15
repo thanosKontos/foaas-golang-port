0e084469a5901628d23e7eba0155e30e62cddacf
Thanos Kontos <tkontos@gmail.com>
2018-02-15 22:07:12 +0200

    Add all xxx/:from fucking routes

--- api_resources.go
+++ api_resources.go
@@ -12,10 +12,68 @@ type ApiResponse struct {
 	Subtitle string `json:"subtitle"`
 }
 
-func GetAnyway(w http.ResponseWriter, r *http.Request) {
+var routesFromToMessage = map[string]string{
+	"/awesome/{from}":     "This is Fucking Awesome.",
+	"/asshole/{from}":     "Fuck you, asshole.",
+	"/bag/{from}":         "Eat a bag of fucking dicks.",
+	"/because/{from}":     "Why? Because fuck you, that's why.",
+	"/bucket/{from}":      "Please choke on a bucket of cocks.",
+	"/bye/{from}":         "Fuckity bye!",
+	"/cool/{from}":        "Cool story, bro.",
+	"/cup/{from}":         "How about a nice cup of shut the fuck up?",
+	"/diabetes/{from}":    "I'd love to stop and chat to you but I'd rather have type 2 diabetes.",
+	"/everyone/{from}":    "Everyone can go and fuck off.",
+	"/everything/{from}":  "Fuck everything.",
+	"/family/{from}":      "Fuck you, your whole family, your pets, and your feces.",
+	"/fascinating/{from}": "Fascinating story, in what chapter do you shut the fuck up?",
+	"/flying/{from}":      "I don't give a flying fuck.",
+	"/fyyff/{from}":       "Fuck you, you fucking fuck.",
+	"/give/{from}":        "I give zero fucks.",
+	"/horse/{from}":       "Fuck you and the horse you rode in on.",
+	"/immensity/{from}":   "You can not imagine the immensity of the FUCK I do not give.",
+	"/life/{from}":        "Fuck my life.",
+	"/looking/{from}":     "Looking for a fuck to give.",
+	"/maybe/{from}":       "Maybe. Maybe not. Maybe fuck yourself.",
+	"/me/{from}":          "Fuck me.",
+	"/mornin/{from}":      "Happy fuckin' mornin'!",
+	"/no/{from}":          "No fucks given.",
+	"/pink/{from}":        "Well, fuck me pink.",
+	"/programmer/{from}":  "Fuck you, I'm a programmer, bitch!",
+	"/retard/{from}":      "You Fucktard!",
+	"/ridiculous/{from}":  "That's fucking ridiculous",
+	"/rtfm/{from}":        "Read the fucking manual!",
+	"/sake/{from}":        "For Fuck's sake!",
+	"/shit/{from}":        "Fuck this shit!",
+	"/single/{from}":      "Not a single fuck was given.",
+	"/thanks/{from}":      "Fuck you very much.",
+	"/that/{from}":        "Fuck that.",
+	"/this/{from}":        "Fuck this.",
+	"/too/{from}":         "Thanks, fuck you too.",
+	"/tucker/{from}":      "Come the fuck in or fuck the fuck off.",
+	"/what/{from}":        "What the fuck‽",
+	"/zayn/{from}":        "Ask me if I give a motherfuck ?!!",
+	"/zero/{from}":        "Zero, thats the number of fucks I give.",
+}
+
+var routesFromCompanyToMessage = map[string]string{
+	"/anyway/{company}/{from}": "Who the fuck are you anyway, %s, why are you stirring up so much trouble, and, who pays you?",
+}
+
+func GetCompanyFromHandler(w http.ResponseWriter, r *http.Request) {
+	matchedRoute, _ := mux.CurrentRoute(r).GetPathTemplate()
 	params := mux.Vars(r)
 	json.NewEncoder(w).Encode(ApiResponse{
-		fmt.Sprintf("Who the fuck are you anyway, %s, why are you stirring up so much trouble, and, who pays you?", params["company"]),
+		fmt.Sprintf(routesFromCompanyToMessage[matchedRoute], params["company"]),
+		fmt.Sprintf("- %s", params["from"]),
+	})
+}
+
+func GetFromHandler(w http.ResponseWriter, r *http.Request) {
+	matchedRoute, _ := mux.CurrentRoute(r).GetPathTemplate()
+	fmt.Println(matchedRoute)
+	params := mux.Vars(r)
+	json.NewEncoder(w).Encode(ApiResponse{
+		routesFromToMessage[matchedRoute],
 		fmt.Sprintf("- %s", params["from"]),
 	})
 }
--- router.go
+++ router.go
@@ -1,14 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"net/http"
 )
+
+func genericError(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(ApiResponse{
+		"622 - All The Fucks",
+		"Server invites you to consider the truly monumental amount of fucks it couldn't give about your request.",
+	})
+}
 
 func GetRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/version", GetVersion).Methods("GET")
 	router.HandleFunc("/operations", GetOperations).Methods("GET")
-	router.HandleFunc("/anyway/{company}/{from}", GetAnyway).Methods("GET")
+	router.HandleFunc("/anyway/{company}/{from}", GetCompanyFromHandler).Methods("GET")
+	router.HandleFunc("/awesome/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/asshole/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/bag/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/because/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/bucket/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/bye/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/cool/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/cup/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/diabetes/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/everyone/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/everything/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/family/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/fascinating/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/flying/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/fyyff/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/give/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/horse/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/immensity/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/life/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/looking/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/maybe/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/me/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/mornin/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/no/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/pink/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/programmer/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/retard/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/ridiculous/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/rtfm/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/sake/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/shit/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/single/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/thanks/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/that/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/this/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/too/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/tucker/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/what/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/zayn/{from}", GetFromHandler).Methods("GET")
+	router.HandleFunc("/zero/{from}", GetFromHandler).Methods("GET")
+
+	router.NotFoundHandler = http.HandlerFunc(genericError)
+	router.MethodNotAllowedHandler = http.HandlerFunc(genericError)
 
 	return router
 }
